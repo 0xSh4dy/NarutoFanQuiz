0a70181fa05601d261479050774a783f2e5601f4
rakshit0011 <rakshit_a@ch.iitr.ac.in>
2021-06-12 19:36:08 +0530

    new commit

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-
+// var app = express.app();
 const connection_url = 'mongodb+srv://admin:BoAoAuyCcujHi0Ln@cluster0.qni0g.mongodb.net/NarutoDB?retryWrites=true&w=majority';
-
+// app.use(app);
 
 mongoose.connect(connection_url,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true});
 const nameSchema = mongoose.Schema({
@@ -21,14 +20,17 @@ const Users = mongoose.model("user",nameSchema);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
-app.get("/",(req,res)=>{
+app.get("/",(req,res,next)=>{
     res.render("login");
 })
-app.get("/api/easy",(req,res)=>{
+app.get("/api/easy",(req,res,next)=>{
     res.sendFile(__dirname+"/easy.json");
 })
-
-app.post("/",(req,res)=>{
+app.post("/quizAnswers",(req,res,next)=>{
+console.log(JSON.stringify(req.body));
+  res.send("Answers saved");
+})
+app.post("/",(req,res,next)=>{
     const nameInp = req.body.PlayerName;
     const diff = req.body.difficulty;
     const num = 12*Math.floor(Math.random())+1;
@@ -46,15 +48,18 @@ app.post("/",(req,res)=>{
             score:0,
             difficulty:diff
         })
-        newName.save();
+        // newName.save();
         res.render("easyQuiz.ejs",{
             Username:nameInp,
             Difficulty:diff
         });
     }
-    else{
+    else if(diff<1 && diff>3){
         res.render("error");
     }
 }
 })
+// app.post("/quizAnswers",(req,res)=>{
+//   res.send("Your answers have been recorded. Thanks for giving the quiz");
+// })
 app.listen(process.env.port|| 4000);
