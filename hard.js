b4276776e4b375f437fcfa97c6ef6ee49490599b
r4ksh1t0011 <rakshitawasthi17@gmail.com>
2021-06-12 16:05:17 +0530

    New commit

---
+++ hard.js
@@ -0,0 +1,87 @@
+const hardQuestions = [{
+    Question1:"What is the name of the Seven tails?",
+    Option1:"Matatabi",
+    Option2:"Shukaku",
+    Option3:"Saiken",
+    Option4:"Chomei",
+    Answer:"Chomei"
+},{
+    Question2:"What is the other name of Kabutowari?",
+    Option1:"The Executioner's Blade",
+    Option2:"Samehada",
+    Option3:"Blast Sword",
+    Option4:"Helmet Splitter",
+    Answer:"Helmet Splitter"
+},{
+    Question3:"Full name of Zabuza?",
+    Option1:"Zabuza Akebino",
+    Option2:"Zabuza Momochi",
+    Option3:"Zabuza Chan",
+    Option4:"Zabuza Shibuki",
+    Answer:"Zabuza Momochi"
+},{
+    Question4:"Why is Kakashi always late?",
+    Option1:"Being challenged by Might Guy",
+    Option2:"Lost in the path of life",
+    Option3:"Visiting the memorial stones",
+    Option4:"Missions",
+    Answer:"Visiting the memorial stones"
+},{
+    Question5:"Which Akatsuki member among the following lived the longest?",
+    Option1:"Madara",
+    Option2:"Kisame",
+    Option3:"Itachi",
+    Option4:"Konan",
+    Answer:"Konan"
+},{
+    Question6:"Who was the reason for Yahiko's death?",
+    Option1:"Danzo ",
+    Option2:"Hanzo of the Salamander",
+    Option3:"Nagato",
+    Option4:"Obito",
+    Answer:"Danzo"
+},{
+    Question7:"When was Naruto premiered first?",
+    Option1:"2001",
+    Option2:"2002",
+    Option3:"2003",
+    Option4:"2004",
+    Answer:"2002"
+},{ 
+    Question8:"Hand signs for Chidori?",
+    Option1:"Monkey, Dragon,Rat,Bird,Ox,Snake,Dog,Tiger,Monkey",
+    Option2:"Monkey, Dragon,Rat,Bird,Ox,Snake,Dog,Monkey,Tiger",
+    Option3:"Monkey, Dragon,Rat,Bird,Ox,Snake,Tiger,Dog,Monkey",
+    Option4:"Monkey, Dragon,Rat,Bird,Snake,Ox,Dog,Tiger,Monkey",
+    Answer:"Monkey, Dragon,Rat,Bird,Ox,Snake,Dog,Tiger,Monkey"
+},{
+    Question9:"Name of the boy who participated in the race, representing the Wasabi family",
+    Option1:"Kyoruko",
+    Option2:"Idate",
+    Option3:"Gosunkoogi",
+    Option4:"Baki",
+    Answer:"Idate"
+},{
+    Question10:"Which one of the following lives in Hoshigakure?",
+    Option1:"Tazuna",
+    Option2:"Idate",
+    Option3:"Sumaru",
+    Option4:"Yukimaru",
+    Answer:"Sumaru"
+},{
+    Question11:"What seal did Orochimaru put on Naruto?",
+    Option1:"Three Pronged Seal",
+    Option2:"Eight Signed Seal",
+    Option3:"Five Signed Seal",
+    Option4:"Five Pronged Seal",
+    Answer:"Five Pronged Seal"
+},{
+    Question12:"Which one of the following was the most evil person?",
+    Option1:"Madara",
+    Option2:"Danzo",
+    Option3:"Itachi",
+    Option4:"Obito",
+    Answer:"Danzo"
+}];
+
+module.exports = {hardQuestions};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const connection_url = 'mongodb+srv://admin:BoAoAuyCcujHi0Ln@cluster0.qni0g.mongodb.net/NarutoDB?retryWrites=true&w=majority';
 mongoose.connect(connection_url,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true});
 const nameSchema = mongoose.Schema({
@@ -11,15 +12,31 @@ const nameSchema = mongoose.Schema({
     difficulty:Number
 })
 const Users = mongoose.model("user",nameSchema);
+
+// Generating random numbers to display the questions and options
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.get("/",(req,res)=>{
     res.render("login");
 })
+app.get("/api/easy",(req,res)=>{
+    res.sendFile(__dirname+"/easy.json");
+})
+
 app.post("/",(req,res)=>{
     const nameInp = req.body.PlayerName;
     const diff = req.body.difficulty;
+    const num = 12*Math.floor(Math.random())+1;
+    let qs = "";
+    if(diff===1){
+        qs = easyQs[num]
+    }
+    if(res.statusCode==404){
+        res.render("error404");
+    }
+    else{
     if(diff>=1 && diff<=3){
         const newName = new Users({
             name:nameInp,
@@ -27,10 +44,14 @@ app.post("/",(req,res)=>{
             difficulty:diff
         })
         newName.save();
-        res.render("quiz.ejs");
+        res.render("easyQuiz.ejs",{
+            Username:nameInp,
+            Difficulty:diff
+        });
     }
     else{
         res.render("error");
     }
+}
 })
 app.listen(process.env.port|| 4000);
